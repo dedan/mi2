68828b36021881476cf1f077c0f109d743a1b0f1
Stephan Gabler <stephan.gabler@gmail.com>
2010-05-25 22:33:31 +0200

    finished 4.1 (pca)

--- ex4/ex4_gr1.m
+++ ex4/ex4_gr1.m
@@ -1,26 +1,43 @@
-%comment
 
 load toypca/pca_data.dat
+res = struct;
+
+
+% remove column means (centering)
+dat = bsxfun(@minus,pca_data,mean(pca_data,1));
 
 % plot the datapoints
-subplot 221
-scatter(pca_data(:,1), pca_data(:,2)); 
-title('original data')
+res(1).dat      = dat;
+res(1).title    = 'original data';
 
 % compute principal components
-dat = bsxfun(@minus,pca_data,mean(pca_data,1));
 [pcs, lambdas]  = eig(cov(dat));
 [throw, idx]    = sort(diag(lambdas),'descend');
 pcs             = pcs(:,idx);
 
 % transform data to pca space
-proj = dat * pcs;
+proj            = dat * pcs;
+res(2).dat      = proj;
+res(2).title    = 'data in pc space';
 
-% datapoints in pc coordinate system
-subplot 222
-scatter(proj(:,1), proj(:,2))
+% backprojection (reconstruction) from pc space
+res(3).dat      = proj * pcs';
+res(3).title    = 'backprojected data (full)';
 
-subplot 223
-[t bla] = princomp(pca_data);
-scatter(bla(:,1), bla(:,2))
+% reconstrucdtion from single pcs
+res(4).dat      = proj(:,1) * pcs(:,1)';
+res(4).title    = 'backprojected data (pc 1)';
+
+res(5).dat      = proj(:,2) * pcs(:,2)';
+res(5).title    = 'backprojected data (pc 2)';
+
+% ploting
+for i = 1:length(res)
+    subplot(length(res),1,i)
+    scatter(res(i).dat(:,1), res(i).dat(:,2));
+    title(res(i).title);
+    if i~=2
+        axis([min(dat(:,1)) max(dat(:,1)) min(dat(:,2)) max(dat(:,2))])
+    end
+end
 
