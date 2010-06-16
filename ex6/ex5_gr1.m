1a3103ee8d18aa844829c9bf125826b8ed815648
Stephan Gabler <stephan.gabler@gmail.com>
2010-06-16 11:49:23 +0200

    finished init part

--- ex6/ex5_gr1.m
+++ ex6/ex5_gr1.m
@@ -2,16 +2,39 @@
 close all
 clear
 
+%% part 1 init
 
+% load the sounds
 load sounds/sound1.dat
 load sounds/sound2.dat
 s = [sound1'; sound2'];
 
-% soundsc(sound1)
-% soundsc(sound2)
-
+% settings 
 N = 2;
 
+% create mixing matrix and mix sources
 A = rand(N);
 x = A*s;
-x = x(:,randperm(length(x)));
+
+% permute columns of data matrix
+x_perm = x(:,randperm(length(x)));
+
+% correlation coefficients
+for i = 1:N
+    c = corrcoef(s(i,:)', x(i,:)');
+    disp(['correlation signal ' int2str(i) ': ' num2str(c(2,1))]);
+end
+
+% center the data
+x = bsxfun(@minus, x, mean(x,2));
+
+% initialize unmixing matrix w randomly
+w = rand(N);
+
+
+%% part 2 optimization
+
+
+
+% soundsc(sound1)
+% soundsc(sound2)
